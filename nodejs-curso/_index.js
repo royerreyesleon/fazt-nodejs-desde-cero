140e5213d5009035576f5cb4037d9b5bc5bee094
Royer Reyes León <royer@rreyes.localdomain>
2021-04-16 14:31:50 -0500

    Curso terminado

---
+++ nodejs-curso/_index.js
@@ -0,0 +1,8 @@
+const math = require ('./math.js');
+console.log("math", math);
+
+/*
+console.log("suma: ",        math.add(1,2));
+console.log("resta: ",       math.substract(1,2));
+console.log("multiplicar: ", math.multiply(1,2));
+console.log("dividir: ",     math.divide(1,0));*/
